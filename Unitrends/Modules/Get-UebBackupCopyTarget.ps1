093b201142c90e576c0c175e153f38cb9ba4ac87
Lucas Schill <lschill@healthedge.com>
2016-11-03 13:05:57 -0400

    Get-UebBackupCopyTargets

---
+++ Unitrends/Modules/Get-UebBackupCopyTarget.ps1
@@ -0,0 +1,38 @@
+﻿<#
+.SYNOPSIS
+Returns a list of backup copy target servers
+.DESCRIPTION
+Uses the Unitrends API to get a list of backup copy targets
+.PARAMETER Name
+The name of the backup copy target
+.EXAMPLE 
+Get-UebBackupCopyTarget
+Returns all UEB backup copy targets
+.EXAMPLE 
+Get-UebBackupCopyTarget -Name "ueb-02"
+Returns the backup copy target with a name of ueb-02
+#>
+Function Get-UebBackupCopyTarget {
+
+    [CmdletBinding()]
+	param (
+
+        [Parameter(Mandatory=$false)]
+        [string]$Name
+
+	)
+
+    $response = (UebGet "api/backup-copy/connected_targets").data
+
+    If($Name){
+
+        $response | Where-Object { $_.name -like $Name }
+
+    }
+    Else{
+        
+        $response
+
+    }
+
+}
