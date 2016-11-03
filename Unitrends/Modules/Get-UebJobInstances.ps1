61d3d3c66d562d0e01cda26e579bfff53cd6bfcf
Lucas Schill <lschill@healthedge.com>
2016-11-03 14:12:30 -0400

    Get-UebJobInstances

---
+++ Unitrends/Modules/Get-UebJobInstances.ps1
@@ -0,0 +1,31 @@
+﻿<#
+.SYNOPSIS
+Gets the instances for the specified job
+.DESCRIPTION
+Uses the Unitrends API to get the instances for the specified job
+.PARAMETER Name
+The name of the job
+.EXAMPLE 
+Get-UebJobInstances -Name "ueb-job"
+Returns the instances associated with the job "ueb-job"
+#>
+Function Get-UebJobInstances {
+
+    [CmdletBinding()]
+	param (
+
+        [Parameter(Mandatory=$true)]
+        [string]$Name
+
+    )
+
+    $j = Get-UebJob -Name $Name
+	$id = $j.id
+	$sid = $j.sid
+    $jobname = $j.name
+
+    $joborder = (Get-UebApi "api/joborders/$id/?sid=$sid").data
+
+    $joborder.instances
+
+}
